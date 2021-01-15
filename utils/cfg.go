7023122d979ca1f245ec81cbc7d30381d753ca08
chengyiwu <chengyiwu@91jkys.com>
2021-01-15 20:56:57 +0800

    ADD split the configs API and the services&apps API

--- utils/cfg.go
+++ utils/cfg.go
@@ -1,7 +1,13 @@
 package utils
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"github.com/coreos/etcd/clientv3"
+	"github.com/golang/glog"
+	"os"
 	"time"
+
 )
 
 // ETCDConfig etcd config
@@ -9,4 +15,30 @@ type ETCDConfig struct {
 	Endpoints []string      `default:"[\"127.0.0.1:2379\"]"`
 	Timeout   time.Duration `default:"5s"`
 	CACert    string
+}
+
+// NewEtcdClient new etcd client
+func (etcd *ETCDConfig) NewEtcdClient() *clientv3.Client {
+	var tlsConfig *tls.Config
+	if etcd.CACert != "" {
+		cert, err := ReadPEMCertificate(etcd.CACert)
+		if err != nil {
+			glog.Errorf("read etcd's cacertfail: %v", err)
+			os.Exit(-1)
+		}
+
+		pool := x509.NewCertPool()
+		pool.AddCert(cert)
+		tlsConfig = &tls.Config{RootCAs: pool}
+	}
+	etcdConfig := clientv3.Config{
+		Endpoints:   etcd.Endpoints,
+		DialTimeout: etcd.Timeout,
+		TLS:         tlsConfig}
+	etcdClient, err := clientv3.New(etcdConfig)
+	if err != nil {
+		glog.Errorf("create etcd clientv3 fail: %v", err)
+		os.Exit(-1)
+	}
+	return etcdClient
 }
